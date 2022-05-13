f03012092a620a883a6904ee980c5dd376973147
loganhansel <loganhansel23@gmail.com>
2022-05-12 20:50:12 -0400

    commit tree 6c31ac335ecf7b3ad19b8071881e67246c9f6a61

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(item){
+    item = item.split(' ');
+    item = item.map(function(word){
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    });
+    item = item.join(' ');
+    return item;
+  })
 }
